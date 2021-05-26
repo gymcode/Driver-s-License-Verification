35fe6455594284f160140d2e005a4540a034f31c
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-05-26 14:35:48 +0000

    initial commit for starter file

---
+++ src/components/NavItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavItem = props => (
+  <li className="nav-item">
+    <a
+      className={`nav-link${props.selected === props.id ? ' active' : ''}`}
+      href="#"
+      onClick={e => {
+        e.preventDefault()
+        props.onClick(props.id)
+      }}
+    >
+      {props.label}
+    </a>
+    <a>
+      hello
+    </a>
+  </li>
+)
+
+export default NavItem
